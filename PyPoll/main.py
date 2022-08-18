83324def0cc957a7b8ca7845561ddfe1509c0ed3
Shaun <gotocooling@gmail.com>
2022-08-19 01:15:18 +1000

    Begin to code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,2 +1,17 @@
 # PyPoll
-print("Hello PyPoll")
+import os
+import csv
+
+totalVotes=0
+scriptpath=os.path.dirname(os.path.realpath('__file__'))
+csvpath=os.path.join(scriptpath,"Resources","election_data.csv")
+with open(csvpath,encoding='utf') as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+    # Skip the first title line
+    next(csvreader)
+
+    # Read each row
+    for row in csvreader:
+        totalVotes=totalVotes+1
+
+print(f'Total Votes: {totalVotes}')
