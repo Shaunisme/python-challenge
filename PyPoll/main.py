11a4d7bf46ba4d8d0b6f9dc22de1c1aec49cba30
Shaun <gotocooling@gmail.com>
2022-08-19 01:24:38 +1000

    Think how to do

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,6 +3,9 @@ import os
 import csv
 
 totalVotes=0
+canditate[]
+number[]
+
 scriptpath=os.path.dirname(os.path.realpath('__file__'))
 csvpath=os.path.join(scriptpath,"Resources","election_data.csv")
 with open(csvpath,encoding='utf') as csvfile:
@@ -14,4 +17,9 @@ with open(csvpath,encoding='utf') as csvfile:
     for row in csvreader:
         totalVotes=totalVotes+1
 
+print('Election Resluts')
+print('----------------------------------')
 print(f'Total Votes: {totalVotes}')
+print('----------------------------------')
+print('----------------------------------')
+print('----------------------------------')
