32d9d2e52e71242928bffb115e204434a872d90e
Shaun <gotocooling@gmail.com>
2022-08-19 11:16:56 +1000

    add comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,33 +6,43 @@ from collections import Counter
 def printline():
         print('----------------------------------')
 
+# init list
 canditate=[]
 
-scriptpath=os.path.dirname(os.path.realpath('__file__'))
-csvpath=os.path.join(scriptpath,"Resources","election_data.csv")
-with open(csvpath,encoding='utf') as csvfile:
-    csvreader=csv.reader(csvfile,delimiter=",")
+# open data file to read
+scriptPath=os.path.dirname(os.path.realpath('__file__'))
+csvPath=os.path.join(scriptPath,"Resources","election_data.csv")
+with open(csvPath,encoding='utf') as csvFile:
+    csvReader=csv.reader(csvFile,delimiter=",")
     # Skip the first title line
-    next(csvreader)
+    next(csvReader)
 
     # Read each row
-    for row in csvreader:
+    for row in csvReader:
         canditate.append(row[2])
     
+    # Call Counter() to sum votes, get a dictionary voteCount
     voteCount=Counter(canditate)
     
+    # Print Results
     print('Election Resluts')
     printline()
 
+    # Calc total votes
     totalVotes=sum(voteCount.values())
     print(f'Total Votes: {totalVotes}')
     printline()
 
+    # Print each one name, percent and vote count
     for name in voteCount.keys():
         voteNumber=voteCount[name]
         print(f'{name}: {round(voteNumber/totalVotes*100,3)}% ({voteNumber})')
     printline()
+
+    # choose the max votes and print winner
     maxVotes=max(voteCount.values())
     winner=[i for i in voteCount.keys() if voteCount[i] == maxVotes]
     print(f'Winner: {winner[0]}')
     printline()
+
+# The end
